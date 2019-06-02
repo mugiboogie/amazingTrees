a905746497701831f0722e790912a07f774c3499
Tabindexing <49012751+Tabindexing@users.noreply.github.com>
2019-06-02 17:17:26 -0400

    c-c-c-changes
    all bad though

--- amazingTrees/Assets/Scripts/Enemy/EnemyAttack.cs
+++ amazingTrees/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -29,6 +29,8 @@ public class EnemyAttack : MonoBehaviour
     private Vector3 hitscanTarget;
     public Animator warningIndicator;
     private string status;
+    float normalAttack = 0.75f;
+    float heavyattack = 0.25f;
 
     void Awake()
     {
@@ -38,11 +40,13 @@ public class EnemyAttack : MonoBehaviour
         enemyController = GetComponent<EnemyController>();
         col = GetComponent<CapsuleCollider>();
         cameraShake = Camera.main.GetComponent<CameraShake>();
-        status = "S";
+        status = "H";
     }
 
     void Update()
     {
+        float random = Random.value;
+        
         anim.SetBool("Attack", false);
 
         if (setAttack)
@@ -54,7 +58,7 @@ public class EnemyAttack : MonoBehaviour
             enemyController.desiredDistance = passiveRange;
         }
 
-        if ((setAttack==true)&& (Vector3.Distance(transform.position, player.position)<=(attackRange+1f))&&(anim.GetCurrentAnimatorStateInfo(0).tagHash != Animator.StringToHash("Attack")) && (anim.GetCurrentAnimatorStateInfo(1).tagHash != Animator.StringToHash("Hit")) && (anim.GetCurrentAnimatorStateInfo(1).tagHash != Animator.StringToHash("KnockUp")))
+        if ((setAttack==true) && (Vector3.Distance(transform.position, player.position)<=(attackRange+1f))&&(anim.GetCurrentAnimatorStateInfo(0).tagHash != Animator.StringToHash("Attack")) && (anim.GetCurrentAnimatorStateInfo(1).tagHash != Animator.StringToHash("Hit")) && (anim.GetCurrentAnimatorStateInfo(1).tagHash != Animator.StringToHash("KnockUp")))
         {
 
             warningIndicator.SetTrigger("Warning");
@@ -89,9 +93,6 @@ public class EnemyAttack : MonoBehaviour
         if(ranged==false)
         {
             //Melee Unit
-            
-            
-            
             Collider[] hitcollider = Physics.OverlapSphere(transform.position + Vector3.up*col.height/2f + transform.forward*attackRange/2f, attackRange/2f);
 
             for (int i = 0; i < hitcollider.Length; i++)
@@ -104,8 +105,6 @@ public class EnemyAttack : MonoBehaviour
                 }
 
             }
-                    
-   
             
         }
         else
@@ -133,37 +132,6 @@ public class EnemyAttack : MonoBehaviour
             }
             
         }
-
-        /*setAttack = false;
-        willAttack = false;
-        float appliedDamage;
-
-        appliedDamage = baseDamage + Random.Range(-damageVariance, damageVariance);
-        anim.SetTrigger("Attack");
-
-        if(ranged == true)
-        {
-            if(hitscan == false)
-            {
-                float projectileHeight = col.height / 2f;
-                GameObject projectileObj = Instantiate(projectile, transform.position + Vector3.up*projectileHeight, transform.rotation);
-                projectileObj.GetComponent<EnemyProjectile>().damage = appliedDamage;
-            }
-            else
-            {
-                //Revise Hitscan attack.
-                RaycastHit hit;
-                Vector3 origin = transform.position + Vector3.up;
-                Vector3 hitscanTarget = player.position + Vector3.up;
-
-                Physics.Raycast(origin, (hitscanTarget - origin).normalized, out hit, Vector3.Distance(origin, hitscanTarget), affectedLayers);
-
-            }
-        }
-        else
-        {
-            anim.SetTrigger("Melee");
-        }*/
     }
 
 
