97f63675b7a7d3cbbeef92080f3ff25d19605ab9
CommanderKyubey <jdrutkowski@gmail.com>
2019-06-23 16:27:37 -0400

    farticles

---
+++ amazingTrees/Assets/ParticleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    public GameObject[] weakHit;
+    public GameObject[] medHit;
+    public GameObject[] heavyHit;
+
+    public void CreateParticle(Vector3 position, float damage)
+    {
+        GameObject setParticle;
+        if (damage < 10f)
+        {
+            setParticle = weakHit[Random.Range(0, weakHit.Length)];
+        }
+        else if ((damage >= 10f) || (damage < 20f))
+        {
+            setParticle = medHit[Random.Range(0, medHit.Length)];
+        }
+        else
+        {
+            setParticle = heavyHit[Random.Range(0, heavyHit.Length)];
+        }
+        Instantiate(setParticle, position, Quaternion.identity);
+    }
+}
