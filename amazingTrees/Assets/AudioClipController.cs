7e00dc408a611070ff184fbcddc8cb49f0188bb2
Eric Trombetti <trombettieric@gmail.com>
2019-07-07 13:35:16 -0500

    Audio Clip Swing

--- amazingTrees/Assets/AudioClipController.cs
+++ amazingTrees/Assets/AudioClipController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class AudioClipController : MonoBehaviour
 {
     public AudioClip[] hit;
+    public AudioClip[] swing;
     private AudioSource audio;
 
     void Awake()
@@ -15,6 +16,12 @@ public class AudioClipController : MonoBehaviour
     public void PlayHit(Vector3 position)
     {
         AudioClip clip = hit[Random.Range(0, hit.Length)];
+        AudioSource.PlayClipAtPoint(clip, position);
+    }
+
+    public void PlaySwing(Vector3 position)
+    {
+        AudioClip clip = swing[Random.Range(0, swing.Length)];
         AudioSource.PlayClipAtPoint(clip, position);
     }
 }
