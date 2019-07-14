76655e40049c676a87271e2b58e93cbdbb886db6
CommanderKyubey <jdrutkowski@gmail.com>
2019-07-14 18:14:42 -0400

    Sounds

--- amazingTrees/Assets/Scripts/Level/DestructablesHealth.cs
+++ amazingTrees/Assets/Scripts/Level/DestructablesHealth.cs
@@ -7,7 +7,10 @@ public class DestructablesHealth : MonoBehaviour
     public float maxHealth = 100f;
     public float currentHealth;
     public GameObject destroyedObj;
+    public AudioClip[] hits;
+    public AudioClip[] dead;
 
+    private bool deathPlayed;
     private Animator anim;
     private PlayerMovement playerMovement;
     private bool playerDead;
@@ -28,7 +31,13 @@ public class DestructablesHealth : MonoBehaviour
     {
      if(currentHealth <= 0f)
         {
-            if(!playerDead)
+            if (deathPlayed == false)
+            {
+                deathPlayed = true;
+                PlayDead(transform.position);
+            }
+
+            if (!playerDead)
             {
                 ObjDying();
             }
@@ -41,8 +50,9 @@ public class DestructablesHealth : MonoBehaviour
 
     public void TakeDamage(float damageValue, string effect)
     {
-           currentHealth -= damageValue;
+            currentHealth -= damageValue;
             currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+            PlayHits(transform.position);
     }
 
     void ObjDying()
@@ -59,5 +69,18 @@ public class DestructablesHealth : MonoBehaviour
         }
         Destroy(gameObject);
         Instantiate(destroyedObj, transform.position, transform.rotation);
+    }
+
+    public void PlayDead(Vector3 position)
+    {
+        AudioClip clip = dead[Random.Range(0, hits.Length)];
+        AudioSource.PlayClipAtPoint(clip, position);
+    }
+
+    public void PlayHits(Vector3 position)
+    {
+        AudioClip clip = hits[Random.Range(0, hits.Length)];
+        //audio.PlayOneShot(clip, 1f);
+        AudioSource.PlayClipAtPoint(clip, position);
     }
 }
