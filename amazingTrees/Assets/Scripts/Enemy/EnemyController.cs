35cfd976a06415e0165b9d8458c15dbdb9433d28
CommanderKyubey <jdrutkowski@gmail.com>
2019-07-21 14:58:54 -0400

    peepee

--- amazingTrees/Assets/Scripts/Enemy/EnemyController.cs
+++ amazingTrees/Assets/Scripts/Enemy/EnemyController.cs
@@ -14,6 +14,7 @@ public class EnemyController : MonoBehaviour {
     private float startMoving;
     private EnemyHealth enemyHealth;
     public GameObject drop;
+    public AudioClip[] aggro;
 
     private EnemyDirector enemyDirector;
 
@@ -28,6 +29,8 @@ public class EnemyController : MonoBehaviour {
     private float maxX;
     private float minZ;
     private float maxZ;
+    private AudioClipController audioClipController;
+    private AudioSource audio;
 
     void Awake()
     {
@@ -44,6 +47,8 @@ public class EnemyController : MonoBehaviour {
         maxX = combatZone.transform.position.x + ((combatZone.size.x - margin)/2f);
         minZ = combatZone.transform.position.z - ((combatZone.size.z - margin)/2f);
         maxZ = combatZone.transform.position.z + ((combatZone.size.z - margin)/2f);
+        audio = GetComponent<AudioSource>();
+        audioClipController = GameObject.FindGameObjectWithTag("AudioController").GetComponent<AudioClipController>();
 
         enemyDirector.AddEnemy(this.gameObject);
         
@@ -218,6 +223,7 @@ public class EnemyController : MonoBehaviour {
         }
         direction.y = 0;
         this.transform.rotation = Quaternion.Lerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+        PlayAggro(transform.position);
     }
 
     void faceDamageOrigin()
@@ -248,6 +254,13 @@ public class EnemyController : MonoBehaviour {
             transform.position = position;
         }
 
+    }
+
+    public void PlayAggro(Vector3 position)
+    {
+        AudioClip clip = aggro[Random.Range(0, aggro.Length)];
+        //audio.PlayOneShot(clip, 1f);
+        AudioSource.PlayClipAtPoint(clip, position);
     }
 
 }
