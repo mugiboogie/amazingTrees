6f7a0f6636a9d9c445e503b8a33bb74cda2553d7
CommanderKyubey <49105243+CommanderKyubey@users.noreply.github.com>
2019-10-06 16:29:41 -0400

    df

--- amazingTrees/Assets/Scripts/Hero/ViviBeamRifle.cs
+++ amazingTrees/Assets/Scripts/Hero/ViviBeamRifle.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class ViviBeamRifle : MonoBehaviour
 {
     private CapsuleCollider collider;
+    private PlayerAttack playerAttack;
+    private Animator bishoujoEyes;
+    private AudioSource audio;
+    private TimeManager timeManager;
+    private Animator playerAnim;
+    private float endTime;
+    private EnemyDirector enemyDirector;
+
+
     public LayerMask levelLayers;
     public Transform player;
+    public AudioClip spellSound;
+    public AudioClip attackSound;
+    public AudioClip hitSound;
+    public GameObject particleHit;
+    public GameObject particleEffect;
 
-    void Start()
+    void Awake()
     {
         collider = GetComponent<CapsuleCollider>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerAttack = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAttack>();
+        audio = GameObject.FindGameObjectWithTag("Player").GetComponent<AudioSource>();
+        playerAnim = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().anim;
+        enemyDirector = GameObject.FindGameObjectWithTag("GameController").GetComponent<EnemyDirector>();
+
+        timeManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<TimeManager>();
     }
 
     void FixedUpdate()
@@ -22,29 +42,59 @@ public class ViviBeamRifle : MonoBehaviour
 
     void Update()
     {
-        Vector3 origin = transform.position;
-        float radius = collider.radius;
-        Vector3 targetDir = transform.forward;
-        float defaultDistance = 9999f;
-        RaycastHit hit;
-
-        if (Physics.SphereCast(origin, radius, targetDir.normalized, out hit, defaultDistance, levelLayers))
+        if ((Input.GetButtonDown("SpellAttack")) && (playerAttack.mana >= playerAttack.manaMax))
         {
-            collider.height = hit.distance;
-        }
+            bishoujoEyes = GameObject.FindGameObjectWithTag("PlayerEffects/BishoujoEyes").GetComponent<Animator>();
+            //AudioSource.PlayClipAtPoint(attackSound, playerAttack.transform.position);
+            audio.PlayOneShot(spellSound, 1f);
+            audio.PlayOneShot(attackSound, 1f);
+            bishoujoEyes.SetTrigger("Activate");
+            timeManager.DoSlowmotion();
+            playerAnim.SetTrigger("Spell");
+            playerAttack.mana = 0f;
+            endTime = Time.time + 5f;
+            StartCoroutine(BeamRifle());
 
-        collider.center = new Vector3(0, 0, collider.height / 2f);
+            Vector3 origin = transform.position;
+            float radius = collider.radius;
+            Vector3 targetDir = transform.forward;
+            float defaultDistance = 9999f;
+            RaycastHit hit;
+
+            if (Physics.SphereCast(origin, radius, targetDir.normalized, out hit, defaultDistance, levelLayers))
+            {
+                collider.height = hit.distance;
+            }
+
+            collider.center = new Vector3(0, 0, collider.height / 2f);
+        }            
     }
 
-    private void OnTriggerEnter(Collider other)
+    IEnumerator BeamRifle()
     {
-
-        if (other.CompareTag("Enemy"))
+        while (Time.time < endTime)
         {
-            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
-
-            enemyHealth.TakeDamage(9999, "S", transform.position);
-
+            if (enemyDirector.enemies.Count > 0)
+            {
+                bool soundPlayed = false;
+                for (int i = 0; i < enemyDirector.enemies.Count; i++)
+                {
+                    if (Vector3.Distance(enemyDirector.enemies[i].transform.position + Vector3.up, playerAttack.transform.position + Vector3.up) < 3f)
+                    {
+                        enemyDirector.enemies[i].GetComponent<EnemyHealth>().TakeDamage(Random.Range(20f, 30f), "U", playerAttack.transform.position);
+                        if (soundPlayed == false)
+                        {
+                            Instantiate(particleHit, playerAttack.transform.position, Quaternion.identity);
+                            audio.PlayOneShot(hitSound, 1f);
+                            soundPlayed = true;
+                        }
+                    }
+                }
+            }
+            Instantiate(particleEffect, playerAttack.transform, false);
+            yield return new WaitForSeconds(.25f);
         }
     }
+
+
 }
